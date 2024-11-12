5ed908360e49202e7f1133eee5f65cbcfe4ddec8
Adrian Borup <adrian@borup.com>
2024-11-12 21:11:59 +0100

    Add JavaParquetSource file

---
+++ wayang-platforms/wayang-java/src/main/java/org/apache/wayang/java/operators/JavaParquetSource.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.wayang.java.operators;
+
+import org.apache.wayang.basic.operators.ParquetSource;
+import org.apache.wayang.core.optimizer.OptimizationContext;
+import org.apache.wayang.core.platform.ChannelDescriptor;
+import org.apache.wayang.core.platform.ChannelInstance;
+import org.apache.wayang.core.platform.lineage.ExecutionLineageNode;
+import org.apache.wayang.core.util.Tuple;
+import org.apache.wayang.java.execution.JavaExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+public class JavaParquetSource extends ParquetSource implements JavaExecutionOperator {
+    private static final Logger logger = LoggerFactory.getLogger(JavaParquetSource.class);
+
+    public JavaParquetSource(String inputUrl) {
+        super(inputUrl);
+    }
+
+    @Override
+    public Tuple<Collection<ExecutionLineageNode>, Collection<ChannelInstance>> evaluate(
+            ChannelInstance[] inputs,
+            ChannelInstance[] outputs,
+            JavaExecutor javaExecutor,
+            OptimizationContext.OperatorContext operatorContext) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Collection<String> getLoadProfileEstimatorConfigurationKeys() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public JavaParquetSource copy() {
+        return new JavaParquetSource(this.getInputUrl());
+    }
+
+    @Override
+    public List<ChannelDescriptor> getSupportedInputChannels(int index) {
+        throw new UnsupportedOperationException(String.format("%s does not have input channels.", this));
+    }
+
+    @Override
+    public List<ChannelDescriptor> getSupportedOutputChannels(int index) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+}
